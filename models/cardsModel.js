fc58f96963cf4c6e3374b55b138681af329ae3a9
Miguel Bugalho <miguel.bugalho@gmail.com>
2023-02-20 14:05:36 +0000

    Added a post endpoint to add cards

--- models/cardsModel.js
+++ models/cardsModel.js
@@ -34,6 +34,47 @@ class Card {
             return { status: 500, result: err };
         }
     }
+
+    static async getById(id) {
+        try {
+            let dbResult = 
+                await pool.query("Select * from cards where crd_id=$1",[id]);
+            let dbCards = dbResult.rows;
+            if (!dbCards.length)
+                return {status:404, result: {msg: "No card found with that identifier"}};
+            let dbCard = dbCards[0];
+            let result = cardFromDB(dbCard);
+            return {status: 200, result: result};
+        } catch (err) {
+            console.log(err);
+            return {status: 500, result: err };
+        }
+    }
+
+    static async save(newCard) {
+        try {
+            let dbResult =
+                await pool.query("Select * from cards where crd_name=$1", [newCard.name]);
+            if (dbResult.rows.length != 0)
+                return {
+                    status: 400, result: [{
+                        location: "body", param: "name",
+                        msg: "That name already exists"
+                    }]
+                };
+            let result =
+                await pool.query(`Insert into cards (crd_name, crd_img_url, crd_lore, 
+                crd_description, crd_level, crd_cost, crd_timeout, crd_max_usage, crd_type)
+                values ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING crd_id`, [newCard.name, newCard.url, newCard.lore,
+                newCard.description, newCard.level, newCard.cost, newCard.timeout,
+                newCard.maxUsage, newCard.type]);
+            return { status: 200, result: {msg:"Inserted a new card", id: result.rows[0].crd_id} };
+        } catch (err) {
+            console.log(err);
+            return { status: 500, result: err };
+        }
+    }
+
 }
 
 
--- routes/cardsRoutes.js
+++ routes/cardsRoutes.js
@@ -13,4 +13,40 @@ router.get('/', async function (req, res, next) {
     }
 });
 
+
+router.get('/:id', async function (req, res, next) {
+    try {
+        console.log("Get card with id " + req.params.id);
+        let result = await Card.getById(req.params.id);
+        res.status(result.status).send(result.result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+});
+
+const { body, validationResult } = require('express-validator');
+router.post("/",
+    body('name').isLength({ min: 4, max: 60 })
+        .withMessage('Name must have between 4 and 60 characters'),
+    body('level').isInt({ min: 0 })
+        .withMessage('Level must be a non negative integer number'),
+    body('type').isInt({ min: 1 })
+        .withMessage('Type must be a positive integer number'),
+    async function (req, res, next) {
+        try {
+            console.log("Save card with name " + req.body.name);
+
+            const valid = validationResult(req);
+            if (!valid.isEmpty()) {
+                return res.status(400).json(valid.array());
+            }
+            let result = await Card.save(req.body);
+            res.status(result.status).send(result.result);
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+    });
+
 module.exports = router;
